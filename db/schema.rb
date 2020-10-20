99c4c9a7cc46bf6c98ea90642f1544a1817f0fe8
moriryooo0110 <the.obvious_simple@icloud.com>
2020-10-20 19:52:04 +0900

    メモのフロントエンド実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_28_124302) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.text "contact"
+    t.text "content"
     t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
